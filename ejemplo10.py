db2dd656a08ae9adb8c26596ce6748281cc3636f
carolaljime21 <carolalvaradojime@gmail.com>
2024-05-06 18:17:32 -0500

    PythonArchivos

--- ejemplo10.py
+++ ejemplo10.py
@@ -1,11 +1,14 @@
 archivo = open("data/atp_tennis.csv", "r")
 
-lineas = archivo.readlines() 
-lineas = [l.split("|") for l in lineas] #Lista 
+lineas = archivo.readlines()
 
-for x in lineas:
-    cadena = """<b>Torneo:</b> %s <br> <b>Ganador:<b> %s""" % (x[0],x[9])
-    print(cadena)
-    archivo_generado = open("data/%s.html" % (x[9]), "w")  #Nombre unico de archivo, deben salir 24362 archivos unicos hasta las 12
-    archivo_generado.writelines("%s\n" % (cadena))
-    archivo_generado.close()
+print(len(lineas))
+
+lineas = [l.split("|") for l in lineas]
+
+for i, x in enumerate(lineas, 1):
+    cadena = """
+            <b>Torneo:</b> %s <br> <b> Ganador: </br> %s
+            """ % (x[0], x[9])
+    with open("data/%s.html" % i, "w") as archivo_generado:
+        archivo_generado.write("%s\n" % cadena)
